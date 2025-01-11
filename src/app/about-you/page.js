3ea824f1f5a8579867b67bc8c0cb97c75403f866
Faiw420 <timdarmstaedter3@gmail.com>
2025-01-11 10:51:52 +0100

    Work in progress message.

--- src/app/about-you/page.js
+++ src/app/about-you/page.js
@@ -14,7 +14,8 @@ export default async function Home() {
       <div className="content-wrapper">
         <div className="content">
           <PageTitle title="Work in Progress" />
-          <p>More info about you coming soon...</p>
+          <p>Hey! You like adventures huh? I haven't configured this page just yet. Please come back later...</p>
+          <p>Meanwhile, you can check out my <a href="/journey" className="text-blue-500 font-bold">journey</a> or <a href="/techstack" className="text-blue-500 font-bold">tech stack</a>.</p>
         </div>
       </div>
     </ScrollArea>
